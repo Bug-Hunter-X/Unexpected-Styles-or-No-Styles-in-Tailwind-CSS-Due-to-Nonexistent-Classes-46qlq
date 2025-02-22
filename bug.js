c9edf9c43ee72ef3566891f67586bd617e09e565
BugHunter-X <askzenai@gmail.com>
2025-02-22 21:02:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+// Example showing a Tailwind CSS class that doesn't exist
+<div class="bg-red-500 p-4 text-white">
+  This div has a red background.
+</div>
+
+// In this example, the class "text-white-500" is not a valid Tailwind CSS class.
+<div class="text-white-500">
+   This text will not be white.
+</div>
+```
